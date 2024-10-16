c25cfc95c13a8041643c419d6f814dcd3a471ce3
Aryan Giri <155383285+aryangiri03@users.noreply.github.com>
2024-10-16 09:47:58 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import joblib
+
+model = joblib.load('spam_classifier.pkl')
+vectorizer = joblib.load('tf-idfvector.pkl')
+
+st.title("Email Spam Classification")
+
+mail_text = st.text_area("Enter the email text:")
+
+if st.button("Classify"):
+    if mail_text:
+        mail_tfidf = vectorizer.transform([mail_text])
+        prediction = model.predict(mail_tfidf)                
+        if prediction[0] == 1:
+            st.error("SPAM.")
+        else:
+            st.success("HAM.")            
+    else:
+        st.warning("Please enter some text to classify.")
